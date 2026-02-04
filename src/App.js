c14942b8d90d7d32835585f3c1b360f474a92a51
Rushikesh Jadhav <rajadhav147@gmail.com>
2026-02-04 21:28:28 +0530

    added fetchcoins function and made basic search ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+  const fetchCoins = async () => {
+    try {
+      const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&names=Bitcoin&symbols=btc&category=layer-1&price_change_percentage=1h%2C%2024h");
+      setCoins(res.data);
+      // console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  fetchCoins();
+}, []);
 
 
   return (
-    <div>
-      <h1>cryppulse</h1>
+    <div className='coin-app'>
+      <div class="coin-search">
+        <h1 class="coin-text">Search a currency</h1>
+        <form>
+          <input type="text" placeholder="Search" class="coin-input"/>
+        </form>
+      </div>
+
     </div>
   );
 }
